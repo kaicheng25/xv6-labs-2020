1da82fe17f4d53c7b11cf9d344dfc40d92903ee3
Ubuntu <ubuntu@localhost.localdomain>
2025-06-19 09:42:52 +0800

    完成了第五次作业

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -43,12 +43,24 @@ sys_sbrk(void)
 {
   int addr;
   int n;
+  struct proc *p;
+  p = myproc();
+  
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  addr = p->sz;
+  if(n>0)
+  {
+    p->sz+=n;
+  }
+  else if(p->sz+n>0)
+  {
+    p->sz = uvmdealloc(p->pagetable,p->sz,p->sz+n);
+  }
+  else return -1;
+  // if(growproc(n) < 0)
+  //   return -1;
   return addr;
 }
 
--- kernel/trap.c
+++ kernel/trap.c
@@ -67,7 +67,20 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  } else if((r_scause()==13) || (r_scause()==15)){
+    char *mem;
+    uint64 va = r_stval();
+    if(PGROUNDUP(p->trapframe->sp)-1<va && va<p->sz && (mem=kalloc())!=0){
+      memset(mem, 0, PGSIZE);
+      if(mappages(myproc()->pagetable, PGROUNDDOWN(va), PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+        printf("lazy alloc: failed to map page\n");
+        kfree(mem);
+        p->killed = 1;
+      }
+    }
+    else p->killed = 1;
+  }
+  else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
